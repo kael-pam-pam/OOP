fd7994efc10666bc85866573c789579abdd88150
kael-pam-pam <talaevpv@gmail.com>
2021-05-03 18:02:13 +0300

    оформил тесты

--- Lab3/Car_tests/Car_tests.cpp
+++ Lab3/Car_tests/Car_tests.cpp
@@ -2,106 +2,143 @@
 #include <catch2/catch.hpp>
 #include "../Car/CCar.h"
 
+
 TEST_CASE("Test class Ccar")
 {
-	SECTION("Default values")
-	{
-		CCar car;
-		REQUIRE(car.IsTurnedOn() == false);
-		REQUIRE(car.GetDirection() == Direction::HOLD);
-		REQUIRE(car.GetSpeed() == 0);
-		REQUIRE(car.GetGear() == 0);
-	}
-	
-	SECTION("TurnOnEngine")
-	{
-		CCar car;
-		REQUIRE(car.TurnOnEngine() == true);
-		REQUIRE(car.IsTurnedOn() == true);
-
-		REQUIRE(car.TurnOnEngine() == true);
-	}
-
-	SECTION("TurnOffEngine")
-	{
-		CCar car;
-		REQUIRE(car.TurnOffEngine() == true);
-		REQUIRE(car.IsTurnedOn() == false);
-
-		REQUIRE(car.TurnOnEngine() == true);
-		REQUIRE(car.SetGear(1) == true);
-		REQUIRE(car.SetSpeed(10) == true);	
-		REQUIRE(car.TurnOffEngine() == false);
-
-		REQUIRE(car.SetGear(0) == true);
-		REQUIRE(car.TurnOffEngine() == false);
-
-		REQUIRE(car.SetSpeed(0) == true);
-		REQUIRE(car.TurnOffEngine() == true);
-	}
-
-	SECTION("SetSpeed")
-	{
-		CCar car;
-		REQUIRE(car.SetSpeed(10) == false);
-		REQUIRE(car.GetSpeed() == 0);
-
-		REQUIRE(car.TurnOnEngine() == true);
-		REQUIRE(car.SetSpeed(10) == false);
-		REQUIRE(car.GetSpeed() == 0);
-
-		REQUIRE(car.SetGear(1) == true);
-		REQUIRE(car.SetSpeed(10) == true);
-		REQUIRE(car.GetSpeed() == 10);
-
-		REQUIRE(car.SetSpeed(30) == true);
-		REQUIRE(car.SetSpeed(31) == false);
-
-		REQUIRE(car.SetSpeed(5) == true);
-
-		REQUIRE(car.SetGear(0) == true);
-		REQUIRE(car.SetSpeed(5) == false);	
-		REQUIRE(car.SetSpeed(4) == true);
+	CCar car;
+	REQUIRE(car.IsTurnedOn() == false);
+	REQUIRE(car.GetDirection() == Direction::HOLD);
+	REQUIRE(car.GetSpeed() == 0);
+	REQUIRE(car.GetGear() == 0);
 		
-		REQUIRE(car.SetSpeed(0) == true);
-		REQUIRE(car.SetGear(-1) == true);
-		REQUIRE(car.SetSpeed(-10) == true);
-		REQUIRE(car.SetSpeed(-20) == true);
-		REQUIRE(car.SetSpeed(-21) == false);
-		REQUIRE(car.SetSpeed(-15) == true);
-		REQUIRE(car.SetSpeed(15) == false);
+	SECTION("Test fuction TurnOnEngine")
+	{
+		//turn on engine in start
+		REQUIRE(car.TurnOnEngine());
+		REQUIRE(car.IsTurnedOn());
+
+		//turn on after turn on
+		REQUIRE(car.TurnOnEngine());
 	}
 
-	SECTION("SetGear")
+	SECTION("Test fuction TurnOffEngine")
 	{
-		CCar car;
-		REQUIRE(car.SetGear(1) == false);
-		REQUIRE(car.GetGear() == 0);
-		REQUIRE(car.SetGear(0) == true);
-		REQUIRE(car.TurnOnEngine() == true);
-		REQUIRE(car.SetGear(1) == true);
-		REQUIRE(car.GetGear() == 1);
+		SECTION("turn off in start")
+		{
+			REQUIRE(car.TurnOffEngine());
+			REQUIRE(car.IsTurnedOn() == false);
+		}
 
-		REQUIRE(car.GetSpeed() == 0);
-		REQUIRE(car.SetGear(-1) == true);
-		REQUIRE(car.SetSpeed(-10) == true);
-		REQUIRE(car.SetGear(-2) == false);
-		REQUIRE(car.SetGear(1) == false);
-		REQUIRE(car.SetGear(0) == true);
-		REQUIRE(car.SetGear(-1) == false);
+		SECTION("turn off in speed and gear")
+		{
+			//turn off in speed and gear
+			REQUIRE(car.TurnOnEngine());
+			REQUIRE(car.SetGear(1));
+			REQUIRE(car.SetSpeed(10));	
+			REQUIRE(car.TurnOffEngine() == false);
 
-		REQUIRE(car.SetSpeed(0) == true);
-		REQUIRE(car.SetGear(1) == true);
-		REQUIRE(car.SetSpeed(25) == true);
-		REQUIRE(car.SetGear(2) == true);
-		REQUIRE(car.SetSpeed(45) == true);
-		REQUIRE(car.SetGear(3) == true);
-		REQUIRE(car.SetSpeed(55) == true);
-		REQUIRE(car.SetGear(4) == true);
-		REQUIRE(car.SetSpeed(85) == true);
-		REQUIRE(car.SetGear(5) == true);
-		REQUIRE(car.SetSpeed(145) == true);
-		REQUIRE(car.SetGear(6) == false);
+			//turn off in 0 gear and speed
+			REQUIRE(car.SetGear(0));
+			REQUIRE(car.TurnOffEngine() == false);
+
+			//turn off in 0 gear and 0 speed
+			REQUIRE(car.SetSpeed(0));
+			REQUIRE(car.TurnOffEngine());
+		}
+	}
+
+	SECTION("Test fuction SetSpeed")
+	{
+		SECTION("set speed in start")
+		{
+			REQUIRE(car.SetSpeed(10) == false);
+			REQUIRE(car.GetSpeed() == 0);
+		}
+		
+		SECTION("set speed without gear")
+		{
+			REQUIRE(car.TurnOnEngine());
+			REQUIRE(car.SetSpeed(10) == false);
+			REQUIRE(car.GetSpeed() == 0);
+		}
+
+		SECTION("set speed in gear")
+		{
+			//increase speed
+			REQUIRE(car.TurnOnEngine());
+			REQUIRE(car.SetGear(1));
+			REQUIRE(car.SetSpeed(10));
+			REQUIRE(car.GetSpeed() == 10);
+			REQUIRE(car.SetSpeed(30));
+
+			//above gear speed
+			REQUIRE(car.SetSpeed(31) == false);
+
+			//reduce speed
+			REQUIRE(car.SetSpeed(5));
+
+			//increase/reduce speed in 0 gear
+			REQUIRE(car.SetGear(0));
+			REQUIRE(car.SetSpeed(5) == false);
+			REQUIRE(car.SetSpeed(4));
+
+			//increase/reduce speed in -1 gear
+			REQUIRE(car.SetSpeed(0));
+			REQUIRE(car.SetGear(-1));
+			REQUIRE(car.SetSpeed(-10));
+			REQUIRE(car.SetSpeed(-20));
+			REQUIRE(car.SetSpeed(-21) == false);
+			REQUIRE(car.SetSpeed(-15));
+			REQUIRE(car.SetSpeed(15) == false);
+		}	
+	}
+
+	SECTION("Test funtion SetGear")
+	{
+		SECTION("set gear in start")
+		{
+			REQUIRE(car.SetGear(1) == false);
+			REQUIRE(car.GetGear() == 0);
+			REQUIRE(car.SetGear(0));
+		}
+
+		SECTION("change gear")
+		{
+			//1 gear
+			REQUIRE(car.TurnOnEngine());
+			REQUIRE(car.SetGear(1));
+			REQUIRE(car.GetGear() == 1);
+
+			//-1 gear
+			REQUIRE(car.GetSpeed() == 0);
+			REQUIRE(car.SetGear(-1));
+			REQUIRE(car.SetSpeed(-10));
+			
+			//not exists gear
+			REQUIRE(car.SetGear(-2) == false);
+			
+			//from -1 to 1 gear
+			REQUIRE(car.SetGear(1) == false);
+			REQUIRE(car.SetGear(0));
+			REQUIRE(car.SetGear(-1) == false);
+			REQUIRE(car.SetSpeed(0) == true);
+			REQUIRE(car.SetGear(1) == true);
+			
+			//1-5 gear
+			REQUIRE(car.SetSpeed(25) == true);
+			REQUIRE(car.SetGear(2) == true);
+			REQUIRE(car.SetSpeed(45) == true);
+			REQUIRE(car.SetGear(3) == true);
+			REQUIRE(car.SetSpeed(55) == true);
+			REQUIRE(car.SetGear(4) == true);
+			REQUIRE(car.SetSpeed(85) == true);
+			REQUIRE(car.SetGear(5) == true);
+			REQUIRE(car.SetSpeed(145) == true);
+			
+			//not exists gear
+			REQUIRE(car.SetGear(6) == false);
+		}
+		
 	}
 
 
